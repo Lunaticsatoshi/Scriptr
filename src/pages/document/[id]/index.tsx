679187f63eb8e0aada5788c55654c549c5a3b114
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-12-12 18:45:35 +0530

    :heavy_plus_sign: Adding types for quill as dev dependency

---
+++ src/pages/document/[id]/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const DocumentPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Text Editor</h1>
+        </div>
+    )
+}
+
+export default DocumentPage
